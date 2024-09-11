340c2ce635cf291fd09d1f5277cafff5745e0df4
leizton <leizton@126.com>
2024-09-12 02:04:17 +0800

    compile

--- examples/preprocess.py
+++ examples/preprocess.py
@@ -4,53 +4,68 @@ from keras.preprocessing.sequence import pad_sequences
 from tqdm import tqdm
 
 
-def gen_data_set(data, seq_max_len=50, negsample=0):
-    data.sort_values("timestamp", inplace=True)
+def gen_data_set(data, seq_max_len=50, negsample=5):
+    negsample = max(1, negsample)
+    data.sort_values('timestamp', inplace=True)
     item_ids = data['movie_id'].unique()
     item_id_genres_map = dict(zip(data['movie_id'].values, data['genres'].values))
-    train_set = []
-    test_set = []
-    for reviewerID, hist in tqdm(data.groupby('user_id')):
-        pos_list = hist['movie_id'].tolist()
-        genres_list = hist['genres'].tolist()
-        rating_list = hist['rating'].tolist()
-
-        if negsample > 0:
-            candidate_set = list(set(item_ids) - set(pos_list))
-            neg_list = np.random.choice(candidate_set, size=len(pos_list) * negsample, replace=True)
+    train_set, test_set = [], []
+    for user_id, user_history in tqdm(data.groupby('user_id')):
+        pos_list = user_history['movie_id'].tolist()
+        genres_list = user_history['genres'].tolist()
+        rating_list = user_history['rating'].tolist()
+        if len(pos_list) < 30:
+            continue
+        candidate_set = list(set(item_ids) - set(pos_list))
+        neg_list = np.random.choice(candidate_set, size=len(pos_list) * negsample, replace=True)
+        test_set_start_index = max(1, int(len(pos_list) * 0.9))
         for i in range(1, len(pos_list)):
-            hist = pos_list[:i]
-            genres_hist = genres_list[:i]
             seq_len = min(i, seq_max_len)
-            if i != len(pos_list) - 1:
-                train_set.append((
-                    reviewerID, pos_list[i], 1, hist[::-1][:seq_len], seq_len, genres_hist[::-1][:seq_len],
-                    genres_list[i],
-                    rating_list[i]))
-                for negi in range(negsample):
-                    train_set.append((reviewerID, neg_list[i * negsample + negi], 0, hist[::-1][:seq_len], seq_len,
-                                      genres_hist[::-1][:seq_len], item_id_genres_map[neg_list[i * negsample + negi]]))
+            item_history = pos_list[:i][::-1][:seq_len]
+            item_genres_history = genres_list[:i][::-1][:seq_len]
+            # 第1个是正样本, 跟着negsample个负样本
+            pos_sample = (user_id, pos_list[i], 1, item_history, seq_len, item_genres_history, genres_list[i], rating_list[i])
+            if i < test_set_start_index:
+                train_set.append(pos_sample)
+                for neg_i in range(negsample):
+                    neg_item_id = neg_list[i * negsample + neg_i]
+                    neg_sample = (user_id, neg_item_id, 0, item_history, seq_len, item_genres_history, item_id_genres_map[neg_item_id], 1)
+                    train_set.append(neg_sample)
             else:
-                test_set.append((reviewerID, pos_list[i], 1, hist[::-1][:seq_len], seq_len, genres_hist[::-1][:seq_len],
-                                 genres_list[i],
-                                 rating_list[i]))
-
+                test_set.append(pos_sample)
     random.shuffle(train_set)
     random.shuffle(test_set)
-
-    print(len(train_set[0]), len(test_set[0]))
-
+    print('train_size=%s, test_size=%s' % (len(train_set), len(test_set)))
     return train_set, test_set
 
 
+def gen_model_input(train_set, user_profile, seq_max_len):
+    train_user_ids = np.array([line[0] for line in train_set])
+    train_item_ids = np.array([line[1] for line in train_set])
+    train_label = np.array([line[2] for line in train_set])
+    train_seq = [line[3] for line in train_set]
+    train_hist_len = np.array([line[4] for line in train_set])
+    train_seq_genres = [line[5] for line in train_set]
+    train_genres = np.array([line[6] for line in train_set])
+    train_seq_pad = pad_sequences(train_seq, maxlen=seq_max_len, padding='post', truncating='post', value=0)
+    train_seq_genres_pad = pad_sequences(train_seq_genres, maxlen=seq_max_len, padding='post', truncating='post', value=0)
+    train_model_input = {
+        'user_id': train_user_ids, 'movie_id': train_item_ids, 'hist_movie_id': train_seq_pad,
+        'hist_genres': train_seq_genres_pad, 'hist_len': train_hist_len, 'genres': train_genres
+    }
+    for key in ['gender', 'age', 'occupation', 'zip']:
+        train_model_input[key] = user_profile.loc[train_model_input['user_id']][key].values
+    return train_model_input, train_label
+
+
 def gen_data_set_sdm(data, seq_short_max_len=5, seq_prefer_max_len=50):
-    data.sort_values("timestamp", inplace=True)
+    data.sort_values('timestamp', inplace=True)
     train_set = []
     test_set = []
-    for reviewerID, hist in tqdm(data.groupby('user_id')):
-        pos_list = hist['movie_id'].tolist()
-        genres_list = hist['genres'].tolist()
-        rating_list = hist['rating'].tolist()
+    for user_id, user_history in tqdm(data.groupby('user_id')):
+        pos_list = user_history['movie_id'].tolist()
+        genres_list = user_history['genres'].tolist()
+        rating_list = user_history['rating'].tolist()
         for i in range(1, len(pos_list)):
             hist = pos_list[:i]
             genres_hist = genres_list[:i]
@@ -58,44 +73,20 @@ def gen_data_set_sdm(data, seq_short_max_len=5, seq_prefer_max_len=50):
             seq_prefer_len = min(max(i - seq_short_len, 0), seq_prefer_max_len)
             if i != len(pos_list) - 1:
                 train_set.append(
-                    (reviewerID, pos_list[i], 1, hist[::-1][:seq_short_len][::-1],
+                    (user_id, pos_list[i], 1, hist[::-1][:seq_short_len][::-1],
                      hist[::-1][seq_short_len:seq_short_len + seq_prefer_len], seq_short_len,
                      seq_prefer_len, genres_hist[::-1][:seq_short_len][::-1],
                      genres_hist[::-1][seq_short_len:seq_short_len + seq_prefer_len], rating_list[i]))
             else:
                 test_set.append(
-                    (reviewerID, pos_list[i], 1, hist[::-1][:seq_short_len][::-1],
+                    (user_id, pos_list[i], 1, hist[::-1][:seq_short_len][::-1],
                      hist[::-1][seq_short_len:seq_short_len + seq_prefer_len], seq_short_len,
                      seq_prefer_len, genres_hist[::-1][:seq_short_len][::-1],
                      genres_hist[::-1][seq_short_len:seq_short_len + seq_prefer_len], rating_list[i]))
-
     random.shuffle(train_set)
     random.shuffle(test_set)
-
-    print(len(train_set[0]), len(test_set[0]))
-
+    print('train_size=%s, test_size=%s' % (len(train_set), len(test_set)))
     return train_set, test_set
-
-
-def gen_model_input(train_set, user_profile, seq_max_len):
-    train_uid = np.array([line[0] for line in train_set])
-    train_iid = np.array([line[1] for line in train_set])
-    train_label = np.array([line[2] for line in train_set])
-    train_seq = [line[3] for line in train_set]
-    train_hist_len = np.array([line[4] for line in train_set])
-    train_seq_genres = np.array([line[5] for line in train_set])
-    train_genres = np.array([line[6] for line in train_set])
-    train_seq_pad = pad_sequences(train_seq, maxlen=seq_max_len, padding='post', truncating='post', value=0)
-    train_seq_genres_pad = pad_sequences(train_seq_genres, maxlen=seq_max_len, padding='post', truncating='post',
-                                         value=0)
-    train_model_input = {"user_id": train_uid, "movie_id": train_iid, "hist_movie_id": train_seq_pad,
-                         "hist_genres": train_seq_genres_pad,
-                         "hist_len": train_hist_len, "genres": train_genres}
-
-    for key in ["gender", "age", "occupation", "zip"]:
-        train_model_input[key] = user_profile.loc[train_model_input['user_id']][key].values
-
-    return train_model_input, train_label
 
 
 def gen_model_input_sdm(train_set, user_profile, seq_short_max_len, seq_prefer_max_len):
@@ -121,13 +112,13 @@ def gen_model_input_sdm(train_set, user_profile, seq_short_max_len, seq_prefer_m
                                             truncating='post',
                                             value=0)
 
-    train_model_input = {"user_id": train_uid, "movie_id": train_iid, "short_movie_id": train_short_item_pad,
-                         "prefer_movie_id": train_prefer_item_pad,
-                         "prefer_sess_length": train_prefer_len,
-                         "short_sess_length": train_short_len, 'short_genres': train_short_genres_pad,
+    train_model_input = {'user_id': train_uid, 'movie_id': train_iid, 'short_movie_id': train_short_item_pad,
+                         'prefer_movie_id': train_prefer_item_pad,
+                         'prefer_sess_length': train_prefer_len,
+                         'short_sess_length': train_short_len, 'short_genres': train_short_genres_pad,
                          'prefer_genres': train_prefer_genres_pad}
 
-    for key in ["gender", "age", "occupation", "zip"]:
+    for key in ['gender', 'age', 'occupation', 'zip']:
         train_model_input[key] = user_profile.loc[train_model_input['user_id']][key].values
 
     return train_model_input, train_label
--- examples/run_youtubednn.py
+++ examples/run_youtubednn.py
@@ -1,154 +1,87 @@
 import os
 import sys
-
-project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(project_dir)
-
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 from collections import Counter
-from deepctr.feature_column import SparseFeat, VarLenSparseFeat
-from deepmatch.models import *
-from deepmatch.utils import sampledsoftmaxloss, NegativeSampler
-from preprocess import gen_data_set, gen_model_input
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.models import Model
 
-if __name__ == "__main__":
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(project_dir)
+from deepctr.feature_column import SparseFeat, VarLenSparseFeat
+from deepmatch.models import *
+from deepmatch.utils import sampledsoftmaxloss, NegativeSampler
+from preprocess import gen_data_set, gen_model_input
+
+
+if __name__ == '__main__':
     # user_id,movie_id,rating,timestamp,title,genres,gender,age,occupation,zip
-    movielens_file = project_dir + "/examples/movielens_sample.txt"
+    movielens_file = project_dir + '/examples/movielens_sample.txt'
     data = pd.read_csvdata = pd.read_csv(movielens_file)
     data['genres'] = list(map(lambda x: x.split('|')[0], data['genres'].values))
-
-    sparse_features = ["movie_id", "user_id",
-                       "gender", "age", "occupation", "zip", "genres"]
+    sparse_features = ['movie_id', 'user_id', 'gender', 'age', 'occupation', 'zip', 'genres']
     SEQ_LEN = 50
 
     # 1.Label Encoding for sparse features,and process sequence features with `gen_date_set` and `gen_model_input`
-
-    # feature_max_idx movie_id 209
-    # feature_max_idx user_id 4
-    # feature_max_idx gender 3
-    # feature_max_idx age 4
-    # feature_max_idx occupation 4
-    # feature_max_idx zip 4
-    # feature_max_idx genres 12
     feature_max_idx = {}
     for feature in sparse_features:
         lbe = LabelEncoder()
         data[feature] = lbe.fit_transform(data[feature]) + 1
         feature_max_idx[feature] = data[feature].max() + 1
+        print('feature_max_idx %s %s' % (feature, feature_max_idx[feature]))
 
-    user_profile = data[["user_id", "gender", "age", "occupation", "zip"]].drop_duplicates('user_id')
+    user_profile = data[['user_id', 'gender', 'age', 'occupation', 'zip']].drop_duplicates('user_id')
 
-    item_profile = data[["movie_id"]].drop_duplicates('movie_id')
+    item_profile = data[['movie_id']].drop_duplicates('movie_id')
 
-    user_profile.set_index("user_id", inplace=True)
+    user_profile.set_index('user_id', inplace=True)
 
-    user_item_list = data.groupby("user_id")['movie_id'].apply(list)
+    user_item_list = data.groupby('user_id')['movie_id'].apply(list)
 
     train_set, test_set = gen_data_set(data, SEQ_LEN, 0)
 
     train_model_input, train_label = gen_model_input(train_set, user_profile, SEQ_LEN)
     test_model_input, test_label = gen_model_input(test_set, user_profile, SEQ_LEN)
-    print("train_model_input")
-    print(train_model_input)
-    print("train_label")
-    print(train_label)
-    print(tf.__version__)
-    sys.exit()
+    for e in train_model_input:
+        print('train_model_input key: %s' % e)
+    print('train_label sum: %s' % np.sum(train_label))
+    print('test_label sum: %s' % np.sum(test_label))
 
     # 2.count #unique features for each sparse field and generate feature config for sequence feature
-
     embedding_dim = 16
-
-    user_feature_columns = [SparseFeat('user_id', feature_max_idx['user_id'], embedding_dim),
-                            SparseFeat("gender", feature_max_idx['gender'], embedding_dim),
-                            SparseFeat("age", feature_max_idx['age'], embedding_dim),
-                            SparseFeat("occupation", feature_max_idx['occupation'], embedding_dim),
-                            SparseFeat("zip", feature_max_idx['zip'], embedding_dim),
-                            VarLenSparseFeat(SparseFeat('hist_movie_id', feature_max_idx['movie_id'], embedding_dim, embedding_name="movie_id"), SEQ_LEN, 'mean', 'hist_len'),
-                            VarLenSparseFeat(SparseFeat('hist_genres', feature_max_idx['genres'], embedding_dim, embedding_name="genres"), SEQ_LEN, 'mean', 'hist_len')
-                            ]
-
+    user_feature_columns = [
+        SparseFeat('user_id', feature_max_idx['user_id'], embedding_dim),
+        SparseFeat('gender', feature_max_idx['gender'], embedding_dim),
+        SparseFeat('age', feature_max_idx['age'], embedding_dim),
+        SparseFeat('occupation', feature_max_idx['occupation'], embedding_dim),
+        SparseFeat('zip', feature_max_idx['zip'], embedding_dim),
+        VarLenSparseFeat(SparseFeat('hist_movie_id', feature_max_idx['movie_id'], embedding_dim, embedding_name='movie_id'), SEQ_LEN, 'mean', 'hist_len'),
+        VarLenSparseFeat(SparseFeat('hist_genres', feature_max_idx['genres'], embedding_dim, embedding_name='genres'), SEQ_LEN, 'mean', 'hist_len')
+    ]
     item_feature_columns = [SparseFeat('movie_id', feature_max_idx['movie_id'], embedding_dim)]
-
     train_counter = Counter(train_model_input['movie_id'])
-    print(train_counter)
     item_count = [train_counter.get(i, 0) for i in range(item_feature_columns[0].vocabulary_size)]
     sampler_config = NegativeSampler('frequency', num_sampled=5, item_name='movie_id', item_count=item_count)
 
     # 3.Define Model and train
-    if tf.__version__ >= '2.0.0':
-        tf.compat.v1.disable_eager_execution()
-    else:
-        K.set_learning_phase(True)
-
-    model = YoutubeDNN(user_feature_columns, item_feature_columns, user_dnn_hidden_units=(64, embedding_dim),
-                       sampler_config=sampler_config)
-    # model = MIND(user_feature_columns, item_feature_columns, dynamic_k=False, k_max=2,
-    #              user_dnn_hidden_units=(64, embedding_dim), sampler_config=sampler_config)
-
-    model.compile(optimizer="adam", loss=sampledsoftmaxloss)
-
-    history = model.fit(train_model_input, train_label,  # train_label,
-                        batch_size=256, epochs=1, verbose=1, validation_split=0.0, )
+    tf.compat.v1.disable_eager_execution()
+    model = YoutubeDNN(
+        user_feature_columns, item_feature_columns,
+        user_dnn_hidden_units=(64, embedding_dim),
+        sampler_config=sampler_config)
+    model.compile(optimizer='adam', loss=sampledsoftmaxloss)
+    history = model.fit(
+        train_model_input, train_label,  # train_label,
+        batch_size=256, epochs=1, verbose=1, validation_split=0.0, )
 
     # 4. Generate user features for testing and full item features for retrieval
     test_user_model_input = test_model_input
-    all_item_model_input = {"movie_id": item_profile['movie_id'].values}
-
+    all_item_model_input = {'movie_id': item_profile['movie_id'].values}
     user_embedding_model = Model(inputs=model.user_input, outputs=model.user_embedding)
     item_embedding_model = Model(inputs=model.item_input, outputs=model.item_embedding)
-
     user_embs = user_embedding_model.predict(test_user_model_input, batch_size=2 ** 12)
     item_embs = item_embedding_model.predict(all_item_model_input, batch_size=2 ** 12)
-
     print(user_embs.shape)
     print(item_embs.shape)
-
-    # 5. [Optional] ANN search by faiss  and evaluate the result
-
-    # import heapq
-    # from collections import defaultdict
-    # from tqdm import tqdm
-    # import numpy as np
-    # import faiss
-    # from deepmatch.utils import recall_N
-    # 
-    # k_max = 2
-    # topN = 50
-    # test_true_label = {line[0]: [line[1]] for line in test_set}
-    # 
-    # index = faiss.IndexFlatIP(embedding_dim)
-    # # faiss.normalize_L2(item_embs)
-    # index.add(item_embs)
-    # # faiss.normalize_L2(user_embs)
-    # 
-    # if len(user_embs.shape) == 2:  # multi interests model's shape = 3 (MIND,ComiRec)
-    #     user_embs = np.expand_dims(user_embs, axis=1)
-    # 
-    # score_dict = defaultdict(dict)
-    # for k in range(k_max):
-    #     user_emb = user_embs[:, k, :]
-    #     D, I = index.search(np.ascontiguousarray(user_emb), topN)
-    #     for i, uid in tqdm(enumerate(test_user_model_input['user_id']), total=len(test_user_model_input['user_id'])):
-    #         if np.abs(user_emb[i]).max() < 1e-8:
-    #             continue
-    #         for score, itemid in zip(D[i], I[i]):
-    #             score_dict[uid][itemid] = max(score, score_dict[uid].get(itemid, float("-inf")))
-    # 
-    # s = []
-    # hit = 0
-    # for i, uid in enumerate(test_user_model_input['user_id']):
-    #     pred = [item_profile['movie_id'].values[x[0]] for x in
-    #             heapq.nlargest(topN, score_dict[uid].items(), key=lambda x: x[1])]
-    #     filter_item = None
-    #     recall_score = recall_N(test_true_label[uid], pred, N=topN)
-    #     s.append(recall_score)
-    #     if test_true_label[uid] in pred:
-    #         hit += 1
-    # 
-    # print("recall", np.mean(s))
-    # print("hr", hit / len(test_user_model_input['user_id']))
